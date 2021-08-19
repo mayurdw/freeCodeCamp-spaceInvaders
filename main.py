5f67c2c057225a3d00b887fbd4b50c44c7b4af7c
Mayur Wadhwani <mayurwadhwanid@gmail.com>
2021-08-19 18:54:01 +1200

    Completed until 1.28 timestamp
    Need to work on collision next

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 # Init pygame
 pygame.init()
@@ -15,10 +16,38 @@ pygame.display.set_icon(icon)
 playerImage = pygame.image.load("battleship.png")
 playerX = 370
 playerY = 480
+playerXChange = 0
+
+# Alien
+alienImage = pygame.image.load("alien.png")
+alienX = random.randint(10, 726)
+alienY = random.randint(74, 140)
+alienXChange = 0.1
+alienYChange = 40
+
+# Alien
+bulletImage = pygame.image.load("bullet.png")
+bulletX = 0
+bulletY = 480
+bulletXChange = 0
+bulletYChange = 0.20
+# Ready - Bullet is not fired
+# Fire - Bullet is fired & travelling
+bulletState = "ready"
 
 
 def player(x, y):
     screen.blit(playerImage, (x, y))
+
+
+def alien(x, y):
+    screen.blit(alienImage, (x, y))
+
+
+def fire_bullet(x, y):
+    global bulletState
+    bulletState = "fire"
+    screen.blit(bulletImage, (x + 16, y + 10))
 
 
 # Game loop
@@ -26,11 +55,51 @@ running = True
 while running:
     # Background
     screen.fill((64, 56, 64))
-    # Player
-    player(playerX, playerY)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        # If key pressed
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                playerXChange = -0.3
+            if event.key == pygame.K_RIGHT:
+                playerXChange = + 0.3
+            if event.key == pygame.K_SPACE:
+                if bulletState == "ready":
+                    bulletX = playerX
+                    fire_bullet(bulletX, bulletY)
+        elif event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                playerXChange = 0
+
+    # Alien movement
+    playerX += playerXChange
+    if playerX <= 10:
+        playerX = 10
+    elif playerX >= 726:
+        playerX = 726
+
+    # Enemy movement
+    alienX += alienXChange
+    if alienX <= 10:
+        alienXChange = 0.3
+        alienY += alienYChange
+    elif alienX >= 726:
+        alienXChange = -0.3
+        alienY += alienYChange
+
+    #  bullet movement
+    if bulletY <= 0:
+        bulletY = 480
+        bulletState = "ready"
+    if bulletState is "fire":
+        fire_bullet(bulletX, bulletY)
+        bulletY -= bulletYChange
+
+    # Player
+    player(playerX, playerY)
+    alien(alienX, alienY)
+
     # Last call
     pygame.display.update()
